648fdb648e89958169ae78c559c277227aa26f3d
Geovanio Sousa <mitozinbr@gmail.com>
2023-04-13 22:42:45 -0300

    while

---
+++ while.py
@@ -0,0 +1,26 @@
+A = 0
+while A != 4:
+    print('====================')
+    print('        MENU        ')
+    print('====================')
+    print('1 - Pizza de Calabresa')
+    print('2 - Pizza de Queijo')
+    print('3 - Pizza de Nutella')
+    print('4 - Encerra ligação')
+    print('                   ')
+    A = int(input('Escolha uma das 4 opção :'))  
+    print('                                  ')
+    if A == 1:
+        print('Pedindo pizza de calabresa. ligando....')
+    elif A == 2:
+        print('Pedindo pizza de queijo. ligando....')
+    elif A == 3:
+        print('Pedindo pizza de nutella. ligando....')
+    elif A == 4:
+        print('Encerrando Chamada. desligando....')
+    else:
+        print('Ah?, Não entendi....')
+    
+
+    
+
